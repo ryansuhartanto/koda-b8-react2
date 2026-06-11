73a58e07852a1702f92cd6b76c6e30770e8247de
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-06-11 13:48:15 +0700

    chore: setup project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+// oxlint-disable-next-line no-unassigned-import
+import "./style.css";
+
+export function App() {
+	return <div></div>;
+}
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import oxfmt from "@kekkon-nexus/config/oxfmt";
+import oxlint from "@kekkon-nexus/config/oxlint";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+	plugins: [react(), tailwindcss()],
+
+	fmt: {
+		...oxfmt,
+	},
+	lint: {
+		extends: [oxlint],
+	},
+	staged: {
+		"*": "vp check --no-error-on-unmatched-pattern",
+	},
+});
