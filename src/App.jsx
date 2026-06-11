c01d5ea2d720ff6f1bb8b3bcf59755ec08792571
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-06-11 14:41:57 +0700

    feat: display searchbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,23 @@ import "./style.css";
 
 const Characters = React.lazy(() => import("./Characters"));
 
+function Search() {
+	return (
+		<div className="flex items-center gap-4 w-full h-14 bg-gray-50 rounded-full shadow-xl">
+			<input
+				type="text"
+				className="flex-1 w-full h-full px-8"
+			/>
+		</div>
+	);
+}
+
 export function App() {
 	return (
 		<>
-			<header></header>
+			<header className="sticky inset-bs-0 max-w-3xl p-4 mx-auto py-8 before:content-[''] before:fixed before:inset-0 before:inset-be-0 before:h-16 before:bg-white before:-z-10">
+				<Search />
+			</header>
 			<main className="max-w-3xl p-4 mx-auto">
 				<React.Suspense fallback={<>Loading...</>}>
 					<Characters />
