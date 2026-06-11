5f77ad9e8ab4bd12a64a288a1a44906c2f1e8ada
Ryan Suhartanto <suhartanto@kekkon.nexus>
2026-06-11 14:47:05 +0700

    feat: search filtering

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 // oxlint-disable-next-line no-unassigned-import
 import "./style.css";
 
 const Characters = React.lazy(() => import("./Characters"));
 
-function Search() {
+function Search({ value, setValue }) {
 	return (
 		<div className="flex items-center gap-4 w-full h-14 bg-gray-50 rounded-full shadow-xl">
 			<input
 				type="text"
 				className="flex-1 w-full h-full px-8"
+				value={value}
+				onInput={(e) => setValue(e.target.value)}
 			/>
 		</div>
 	);
 }
 
 export function App() {
+	const [search, setSearch] = useState("");
+
 	return (
 		<>
 			<header className="sticky inset-bs-0 max-w-3xl p-4 mx-auto py-8 before:content-[''] before:fixed before:inset-0 before:inset-be-0 before:h-16 before:bg-white before:-z-10">
-				<Search />
+				<Search
+					value={search}
+					setValue={setSearch}
+				/>
 			</header>
 			<main className="max-w-3xl p-4 mx-auto">
 				<React.Suspense fallback={<>Loading...</>}>
-					<Characters />
+					<Characters
+						filterFn={(results) =>
+							results.filter(({ name }) =>
+								name.toLowerCase().includes(search.toLowerCase()),
+							)
+						}
+					/>
 				</React.Suspense>
 			</main>
 		</>
